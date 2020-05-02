ba4a8a8d0e11dcc614b99f14f84e4722c8ba2590
Alon Braymok <alon@ludus.ac>
2020-05-02 13:47:59 +0300

    remove unudes files

--- app.js
+++ app.js
@@ -4,16 +4,12 @@ const mongoose = require("mongoose");
 
 var cors = require("cors");
 
-const userRoutes = require("./routes/user");
-
 const { DB_CONNECT } = require("./config");
 
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json()); // application/json
-
-app.use(userRoutes);
 
 mongoose
   .connect(DB_CONNECT)
